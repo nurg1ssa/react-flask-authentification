d89b4234832d8d3b2301328e2adb8c584d6f3b56
nurg1ssa <nurike2001@mail.ru>
2024-02-18 20:45:35 +0100

    register form

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
         <Routes>
             <Route path="/" element={<LandingPage/>} />
             <Route path="/login"  element={<LoginPage/>} />
+            <Route path="/register"  element={<RegisterPage/>} />
             <Route element={<NotFound/>} />
         </Routes>
     </BrowserRouter>
